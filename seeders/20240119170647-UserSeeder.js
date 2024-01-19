a9808bbb2a478a81681649368a6bcb2df8e9a6ab
luckylaurens15 <lucky.laurens@binus.ac.id>
2024-01-20 00:17:21 +0700

    config seeder

---
+++ seeders/20240119170647-UserSeeder.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert(
+      'Users', 
+        [
+          {
+            full_name: "Lucky Laurens",
+            email: "luckylaurens1511@gmail.com",
+            phone_number : "082290100141",
+            password : "password",
+            date_of_birth : "2002-11-15"
+          },
+          {
+            full_name: "Rizki Putra Awali",
+            email: "rizkiputrawawali@gmail.com",
+            phone_number : "08221231231",
+            password : "password",
+            date_of_birth : "1988-01-01"
+          }
+        ], {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
