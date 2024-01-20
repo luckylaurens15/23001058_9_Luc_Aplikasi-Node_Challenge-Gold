7d9395955c1a158630c5742ce17f03e8b31204d3
luckylaurens15 <lucky.laurens@binus.ac.id>
2024-01-20 12:14:01 +0700

    modify login and added signup using async await

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,14 +10,38 @@ class UserController {
             email: req.body.email,
             password: req.body.password,
         };
-    
-        const auth = await User.findOne({ where: { email: reqUser.email , password: reqUser.password} });
-        
-        if(auth == null){
-            res.send(`User with email ${reqUser.email} is not found or the password is incorect.`);
+
+        try {
+            const auth = await User.findOne({ where: { email: reqUser.email , password: reqUser.password} });
+            if(auth == null){
+                res.send(`User with email ${reqUser.email} is not found or the password is incorect.`);
+            }
+            else {
+                res.send(`Hi ${auth.full_name}, Welcome Back to BingleShop!`);
+            }
         }
-        else {
-            res.send(`Hi ${auth.full_name}, Welcome to BingleShop!`);
+        catch(err) {
+            res.send(err);
+        }
+    }
+
+    static signupPage(req, res) {
+        res.send("Ini signup page");
+    }
+
+    static async signupUser(req, res) {
+        try {
+            const newUser = await User.create({
+                full_name: req.body.fullName,
+                email: req.body.email,
+                phone_number: req.body.phoneNumber,
+                date_of_birth: req.body.dob,
+                password: req.body.password,
+            });
+            res.send(`Hi ${newUser.full_name}, Welcome to BingleShop!`);
+        }
+        catch(err) {
+            res.send(err);
         }
     }
 }
