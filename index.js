92735d1114154e4f5db1aad56320c80b3a0d8165
luckylaurens15 <lucky.laurens@binus.ac.id>
2024-01-20 11:26:34 +0700

    edit seeder dan menambahkan comment di index.js

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const port = 3000;
 
 const userRouter = require("./router/userRouter.js");
 
-app.use(express.json());
+app.use(express.json()); // for reading body request in json format
 
 app.get("/", (req, res) => {
     res.send('Welcome to BingleShop Landing Page!')
 });
 
 app.use("/users", userRouter);
-
-
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
