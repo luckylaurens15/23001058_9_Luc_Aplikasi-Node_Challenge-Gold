4b12b58e7ea9dbfa338dd996caa13224c20bb3d6
luckylaurens15 <lucky.laurens@binus.ac.id>
2024-01-22 22:05:03 +0700

    router and controller for order

--- router/orderRouter.js
+++ router/orderRouter.js
@@ -4,7 +4,27 @@ const orderRouter = express.Router();
 const {OrderController} = require("../controllers/orderController");
 
 orderRouter
-    .route("/");
+    .route("/")
+    .get(OrderController.getAllOrder);
 
+orderRouter
+    .route("/order/:id")
+    .get(OrderController.getAnOrder);
+
+orderRouter
+    .route("/user/:id")
+    .get(OrderController.getUserOrder);
+
+orderRouter
+    .route("/new-order")
+    .get(OrderController.newOrderPage)
+    .post(OrderController.createNewOrder);
+
+
+// Update Status Note: Status must be ={On Process, Delivered, Done, Cancelled}
+orderRouter
+    .route("/update-status/:id")
+    .get(OrderController.updateStatusPage)
+    .put(OrderController.updateStatus);
 
 module.exports = orderRouter;
